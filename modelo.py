af020c82d2ad529dd9f2c480d9d79ff812557990
JoaoPeAbreu <joao16abreu@gmail.com>
2022-10-03 14:18:18 -0300

    Modelo terminado

--- modelo.py
+++ modelo.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import date
 import os
 app = Flask(__name__)
 CORS(app)
 caminho = os.path.dirname(os.path.abspath(__file__)) 
-arquivobd = os.path.join(caminho, "pessoas.db")
+arquivobd = os.path.join(caminho, "tabelas.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{arquivobd}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db= SQLAlchemy(app)
@@ -17,7 +18,7 @@ class Pessoa(db.Model):
     senha = db.Column(db.String(254))
 
     def __str__(self):
-        return self.nome + "[id="+str(self.id)+ "], " +\
+        return self.nome + "[id=" + str(self.id) + "], " +\
             self.email + ", " + self.senha
 
     def json(self):
@@ -39,14 +40,15 @@ class Emoji(db.Model):
     classificacao = db.Column(db.String(254))
 
     def __str__(self):
-        return self.pessoa + "[id="+str(self.id)+ "], " +\
-            self.datacriacao + ", " + self.nomeemoji + ", " + self.representacao +\
+        return str(self.pessoa) + "[id=" + str(self.id)+ "], " +\
+            str(self.datacriacao) + ", " + self.nomeemoji + ", " + self.representacao +\
                 self.fotoemoji + ", " + self.classificacao
     
     def json(self):
         return {
             "id": self.id,
-            "pessoa": self.pessoa,
+            "pessoa_id": self.pessoa_id,
+            "pessoa": self.pessoa.json(),
             "datacriacao": self.datacriacao,
             "nomeemoji": self.nomeemoji,
             "representacao": self.representacao,
@@ -54,57 +56,59 @@ class Emoji(db.Model):
             "classificacao": self.classificacao
         }
 
-class Gradico(db.Model):
-    id = id = db.Column(db.Integer, primary_key=True)
-    legenda = db.Column(db.String(254))
-    x = db.Column(db.Date)
-    y = db.Column(db.String(254))
-
-    def __str__(self):
-        return self.legenda + "[id="+str(self.id)+ "], " +\
-            self.x + ", " + self.y 
-    
-    def json(self):
-        return {
-            "id": self.id,
-            "legenda": self.legenda,
-            "x": self.x,
-            "y": self.y
-        }
-
 class Sugestao(db.Model):
     id = id = id = db.Column(db.Integer, primary_key=True)
-    id_ = db.Column(db.Integer(254))
     catalogacao = db.Column(db.String(254))
     ideia = db.Column(db.String(254))
 
     def __str__(self):
-        return self.id + "[id="+str(self.id)+ "], " +\
+        return "[id=" + str(self.id)+ "], " +\
             self.catalogacao + ", " + self.ideia
 
     def json(self):
         return {
-            "id": self.id,
             "catalogacao": self.catalogacao,
             "ideia": self.ideia
         }
 
-"""class Pessoa(db.Model):
-    # atributos da pessoa
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(254))
-    email = db.Column(db.String(254))
-    nome_foto = db.Column(db.Text)
+if __name__ == "__main__":
+    if os.path.exists(arquivobd):
+        os.remove(arquivobd)
 
-    # método para expressar a pessoa em forma de texto
-    def __str__(self):
-        return self.nome + "[id="+str(self.id)+ "], " +\
-            self.email + ", " + self.nome_foto
-    # expressao da classe no formato json
-    def json(self):
-        return {
-            "id": self.id,
-            "nome": self.nome,
-            "email": self.email,
-            "nome_foto": self.nome_foto
-        }"""
+    db.create_all()
+
+    p1 = Pessoa(nome = "Akemi", email = "akemi.shibukawa@gmail.com", senha = "antilopes1galopantes")
+    p2 = Pessoa(nome = "João", email = "joao.abreu@gmail.com", senha = "luzes1para2o3mundo")
+
+    db.session.add(p1)
+    db.session.add(p2)
+    db.session.commit()
+
+    print(".....................................")
+    print(p1)
+    print(p2)
+    print(".....................................")
+
+    e1 = Emoji(pessoa = p1, datacriacao = date(2022, 10, 3), nomeemoji = "coelho", representacao = "O emoji representa um coelho femea", classificacao = "Animais", fotoemoji= 'sla')
+    e2 = Emoji(pessoa = p2, datacriacao = date(2022, 10, 2), nomeemoji = "pinheiro", representacao = "O emoji representa o pinheiro que fica na minha casa", classificacao = "Planta", fotoemoji= 'sla2')
+
+    db.session.add(e1)
+    db.session.add(e2)
+    db.session.commit()
+
+    print(".....................................")
+    print(e1)
+    print(e2)
+    print(".....................................")
+
+    s1 = Sugestao(catalogacao = "Planta", ideia = "Samambaia")
+    s2 = Sugestao(catalogacao = "Expressões", ideia = "Sorridente")
+
+    db.session.add(s1)
+    db.session.add(s2)
+    db.session.commit()
+
+    print(".....................................")
+    print(s1)
+    print(s2)
+    print(".....................................")
